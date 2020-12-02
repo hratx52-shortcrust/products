06fbf0cefb7e10ee55c58b992fac02608c035925
Jesse Pye <jessepye@gmail.com>
2020-12-02 15:26:03 -0600

    product_style_skus complete

--- product_style_photos-schema.sql
+++ product_style_photos-schema.sql
@@ -5,15 +5,24 @@
 
 DROP TABLE IF EXISTS product_style_photos;
 
+-- The problem with this csv is that the id column cannot be trusted
+-- there is a duplicate key
+-- ERROR:  duplicate key value violates unique constraint "product_style_photos_pkey"
+-- DETAIL:  Key (photo_id)=(357) already exists.
+-- so we make a dummn column, photo_id_from_csv, and delete it after the copy command
+
 CREATE TABLE product_style_photos (
-  photo_id INT NOT NULL PRIMARY KEY,
+  photo_id SERIAL NOT NULL PRIMARY KEY,
+  photo_id_from_csv INT,
   style_id INT NOT NULL,
   url TEXT,
   thumbnail_url TEXT,
   FOREIGN KEY (style_id) REFERENCES product_styles(style_id)
 );
 
-COPY product_style_photos(photo_id, style_id, url, thumbnail_url)
+COPY product_style_photos(photo_id_from_csv, style_id, url, thumbnail_url)
 FROM '/home/jesse/Code/hratx52/SDC/SDC Application Data - Atelier Project/photos.csv'
 DELIMITER ','
 CSV HEADER;
+
+ALTER TABLE product_style_photos DROP COLUMN photo_id_from_csv;
---
+++ product_style_skus-schema.sql
@@ -0,0 +1,19 @@
+-- sudo -u postgres psql -d SDC < product_style_skus-schema.sql
+
+-- switch to SDC database
+\c SDC;
+
+DROP TABLE IF EXISTS product_style_skus;
+
+CREATE TABLE product_style_skus (
+  sku_id SERIAL NOT NULL PRIMARY KEY,
+  style_id INT NOT NULL,
+  size TEXT,
+  quantity INT,
+  FOREIGN KEY (style_id) REFERENCES product_styles(style_id)
+);
+
+COPY product_style_skus(sku_id, style_id, size, quantity)
+FROM '/home/jesse/Code/hratx52/SDC/SDC Application Data - Atelier Project/skus.csv'
+DELIMITER ','
+CSV HEADER;
