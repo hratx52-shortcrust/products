2e1cf0f10d660c640f371f31f412f19b364b2502
Jesse Pye <jessepye@gmail.com>
2020-12-02 12:10:46 -0600

    finished products-schema.sql

---
+++ products-schema.sql
@@ -0,0 +1,21 @@
+-- sudo -u postgres psql -d SDC < products-schema.sql
+
+-- switch to SDC database
+\c SDC;
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+  product_id INT NOT NULL,
+  name TEXT NOT NULL,
+  slogan TEXT,
+  description TEXT,
+  category TEXT,
+  default_price DECIMAL,
+  PRIMARY KEY (product_id)
+);
+
+COPY products(product_id, name, slogan, description, category, default_price)
+FROM '/home/jesse/Code/hratx52/SDC/SDC Application Data - Atelier Project/product-fixed.csv'
+DELIMITER ','
+CSV HEADER;
