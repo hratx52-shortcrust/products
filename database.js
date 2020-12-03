d11f8dfd55517c04b4fe52c7b6cca09d69adf44f
Jesse Pye <jessepye@gmail.com>
2020-12-03 16:06:34 -0600

    finished getProduct db and router functions (singluar)

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const client = new Client({
 
 client.connect();
 
-const getProducts = async function(page = 1, count = 5)  {
+const getProducts = async function(page = 1, count = 5) {
   if (page < 1) {
     return Promise.reject(new Error('Page must be greater than 0'));
   }
@@ -35,8 +35,39 @@ const getProducts = async function(page = 1, count = 5)  {
   // second value is OFFSET
   const values = [count, count*(page - 1)];
 
-  const res = await client.query(text, values);
-  return res.rows;
+  const products = await client.query(text, values);
+  return products.rows;
 };
 
-module.exports = { getProducts };
+const getProduct = async function(id) {
+  let text = `
+    SELECT
+      product_id AS id,
+      name,
+      slogan,
+      description,
+      category,
+      default_price
+    FROM products
+    WHERE product_id=$1;
+  `;
+
+  let values = [id];
+
+  const res = await client.query(text, values);
+  let itemInfo = res.rows[0];
+
+  // TODO: can we send the second query without having to wait for the first one to finish?
+  text = `
+    SELECT
+      feature_name AS feature,
+      feature_value AS value
+    FROM product_features
+    WHERE product_id=$1;
+  `
+  const features = await client.query(text, values);
+  itemInfo['features'] = features.rows;
+  return itemInfo;
+};
+
+module.exports = { getProducts, getProduct };
--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  /*
+    The stuff after a ? in a URL are called 'parameters'
+    but express uses the word 'params' to refer to the stuff after a ':', for example:
+    /products/:product_id
+    so that a user can perform a GET on /products/3
+    which sets req.params.product_id = 3
+    The stuff after '?', e.g. /products?page=2&results=5
+    is referred to by express as 'query'
+    From the above example, this would set req.query.page = 2; and req.query.results = 5
+  */
   db.getProducts(req.query.page, req.query.results)
     .then( products => {
       res.send(products);
@@ -24,7 +34,14 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/products/:product_id', (req, res) => {
-  res.send(`Will return information about product ${req.params.product_id}`);
+  db.getProduct(req.params.product_id)
+    .then( product => {
+      res.send(product);
+    })
+    .catch( err => {
+      console.log('Error when sending query for product with id ' + req.params.product_id + ':', err);
+      res.status(500).send(err);
+    });
 });
 
 app.get('/products/:product_id/styles', (req, res) => {
